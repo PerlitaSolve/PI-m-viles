5234f4ec11cce03b6606c7a69cc4fc165abbaf74
Daniyel19084 <124051408@upq.edu.mx>
2025-11-21 16:53:24 -0600

    avances mis eventos

--- redsocial/screens/MisEventos.js
+++ redsocial/screens/MisEventos.js
@@ -11,7 +11,7 @@ export default function MisEventos(){
         <View style={styles.separador}>
             <Image
             style={styles.logo}
-            source={require('../assets/LogoPI.png')}
+            source={require('../assets/profile.png')}
            
             />
 
@@ -26,9 +26,31 @@ export default function MisEventos(){
         <ScrollView>
         <View style={styles.Cuadroevento}>
          <View style={styles.Cuadrointerno}>
+
+            <View style={styles.SupPart}>
+
+                <Text> hola</Text>
+
+            </View>
+            <View style={styles.SupMed}>
+                <View>
+                <Text style={styles. textoInternotitulo}>Nombre del Evento</Text>
+                <Text style={styles. textoInterno}>Descripcion de Evento</Text>
+                </View>
+
+                <View>
+                    <Text>HOLAAA</Text>
+                </View>
+            </View>
+            <View style={styles.separadorbotones}>
+       
+                <Pressable style={styles.botonVer}>
+                    <Text style={styles.textoBoton}>VER</Text>
+                </Pressable>  
                 <Pressable style={styles.boton}>
                     <Text style={styles.textoBoton}>CANCELAR EVENTO</Text>
-                </Pressable>            
+                </Pressable>                      
+            </View>
 
             </View>
         </View>
@@ -102,22 +124,22 @@ const styles = StyleSheet.create({
         borderRadius:30,
         zIndex:10,
     },
-    Cuadrointerno:{
+    Cuadrointerno:{ //
         width:350,
         height:550,
         backgroundColor:'#dbdbdbff',
         alignItems:'center',
-        alignContent:'center',
         marginTop:25,
         borderRadius:20, 
         zIndex:11,
-        shadowOpacity:{
-
+        shadowColor:'#7e7c7cff',
+        shadowOffset:{
+            width:0,
+            height:3,
         }
     },
 boton:{
     backgroundColor:'#abb6ffff',
-    marginLeft:'30%',
     paddingVertical:12,
     width:150,
     borderRadius:20,
@@ -135,6 +157,60 @@ textoBoton:{
     color:'#ffffffff',
     fontSize:15,
     fontWeight:'550',
-},
+},  
+    separadorbotones:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',  
+        
+        marginTop:10,
+        gap:20,
+    },
+    botonVer:{
+    backgroundColor:'#aeaeaeff',
+    paddingVertical:12,
+    width:150,
+    borderRadius:20,
+    elevation:5,
+    shadowColor:'#00000056',
+    shadowOffset:{
+        width:0,
+        height:3,
+    },
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:20,        
+    },
+    SupPart:{
+        width:'80%',
+        height:'35%',
+        backgroundColor:'#7e2c2cff',
+        marginTop:30,
+        marginBottom:15,
+        borderRadius:20,
+    },
+    SupMed:{
+        width:'80%',    
+        height:'35%',
+       // backgroundColor:'#dbdbdbff',
+        backgroundColor:'#17c38fff',    
+        borderRadius:20,
+    },
+    textoInternotitulo:{
+        fontSize:17,
+        color:'#fff',
+        marginLeft:8,
+        fontWeight:'550',
+        marginTop:5,    
+      //  justifyContent:'flex-start' ,
+    },
+     textoInterno:{
+        fontSize:14,
+        color:'#fff',
+        marginLeft:8,
+        fontWeight:'550',
+        marginTop:5,     
+       // justifyContent:'flex-end' ,
+    },   
 
 })
