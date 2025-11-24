905946b974c6dc33a8da96022c8856fee5a2d673
PerlitaSolve <124049201@upq.edu.mx>
2025-11-23 22:48:03 -0600

    Olvidar Contraseña completa

--- redsocial/App.js
+++ redsocial/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
+import Sesion from './screens/Sesion';
 import Registro from './screens/Registro';
 import MisEventos from './screens/MisEventos';
 import Notificaciones from './screens/Notificaciones';
+import Contra from './screens/Contra';
+
 
 export default function App() {
   return (
-    <Notificaciones/>
+    <Contra/>
   );
 }
 
