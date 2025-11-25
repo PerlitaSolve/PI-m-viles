bc63c2eee03826612b643cab42cc2cb34397b0e8
PerlitaSolve <124049201@upq.edu.mx>
2025-11-24 18:16:53 -0600

    Editar Perfil Completo

--- redsocial/App.js
+++ redsocial/App.js
@@ -6,11 +6,13 @@ import Registro from './screens/Registro';
 import MisEventos from './screens/MisEventos';
 import Notificaciones from './screens/Notificaciones';
 import Contra from './screens/Contra';
+import Perfil from './screens/Perfil';
+import EdPerfil from './screens/EdPerfil';
 
 
 export default function App() {
   return (
-    <Contra/>
+    <EdPerfil/>
   );
 }
 
