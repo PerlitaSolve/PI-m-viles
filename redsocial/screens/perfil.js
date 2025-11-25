87973159202ce1225e22edc337abb93168970ab8
PerlitaSolve <124049201@upq.edu.mx>
2025-11-24 18:16:37 -0600

    Perfil completo

---
+++ redsocial/screens/perfil.js
@@ -0,0 +1,152 @@
+import { StyleSheet, Text, View, ImageBackground, TextInput, Image, Pressable, ScrollView } from 'react-native'
+import React from 'react'
+
+
+
+export default function Perfil() {
+  return (
+    <ScrollView>
+    <ImageBackground
+      source={require('../assets/Fondo4.png')}
+      style={styles.fondo}
+    >
+        <View style={{position:'absolute', top:40, left:20}}>
+          <Image source={require('../assets/LogoPI.png')} 
+          style={styles.logo}              
+          />
+        </View>
+          <Text style={styles.titulo}>MI PERFIL</Text>
+        
+
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.text2}>---------------------------------------------------</Text>
+
+          <View style={{alignItems:'center', justifyContent:'center', marginBottom:20, marginTop:20}}>
+            <Image source={require('../assets/Lperfil.png')}
+            style={styles.foto}/>
+          </View>
+
+          <Text style={styles.text2}>Nombre de Usuario</Text>
+          <Text style={styles.text2}>---------------------------------------------------</Text>
+        </View>
+
+
+        <View style={styles.cuadro}>
+          <View>
+              <Text style={styles.text}>Nombre</Text>
+              <TextInput placeholder='Nombre Completo' style={styles.input}></TextInput>
+              
+              <Text style={styles.text}>Grupo</Text>
+              <TextInput placeholder='Grupo' style={styles.input}></TextInput>
+
+              <Text style={styles.text}>Telefono</Text>
+              <TextInput placeholder='Telefono' style={styles.input} keyboardType='numeric'></TextInput>
+
+              <Text style={styles.text}>Correo Electrónico</Text>
+              <TextInput placeholder='@correo' style={styles.input} ></TextInput>
+
+              <Pressable style={styles.boton}>
+                  <Text style={styles.bText}>EDITAR PERFIL</Text>
+              </Pressable>
+
+          </View>
+        </View>
+
+
+      </View>
+    </ImageBackground>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+    logo:{
+        height:60,
+        width:90,
+        zIndex:1,
+        marginTop:0,
+        marginBottom:20,
+        flexDirection:'flex-start',
+    },
+    titulo:{
+      fontSize:20,
+      color:'#ffffffff',
+      alignItems:'center',
+      marginBottom: 5,
+      fontFamily:'Josefin Sans',
+      justifyContent:'center',
+      textAlign:'center',
+      padding: 30,
+      marginTop:30,
+    },
+     fondo:{
+        flex:1,
+        resizeMode:'cover',
+        justifyContent:'center',
+        alignItems:'center',
+        height:'100%',
+        width:'100%',
+    },
+    foto:{
+      height: 120,
+      width:120,
+      alignItems:'center',
+      justifyContent:'center',
+      marginBottom:20,
+    },
+    cuadro:{
+        backgroundColor:'#ffffffc7',
+        padding:20,
+        borderRadius:10,
+        width:300,
+        alignItems:'center',
+    },
+    input:{
+        borderWidth:1,
+        borderColor:'#b4b4b4ff',
+        backgroundColor:'#d4d4d4ff',
+        borderRadius:10,
+        padding:10,
+        marginBottom:10,
+        fontFamily:"Instrument Sans",
+        paddingVertical:8,
+        paddingHorizontal:40,
+        marginTop:10,
+    },
+    text:{
+        fontSize:16,
+        color:'#222e6aff',
+        alignItems:'flex-start',
+        marginBottom:5,
+        fontFamily:'Josefin Sans',
+    },
+    boton:{
+        color:'#fff',
+        fontSize:14,
+        fontFamily:"Instrument Sans",
+        backgroundColor:'#80A0EB',
+        borderRadius:10,
+        marginTop:20,
+        textAlign:'center',
+        height:45,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    bText:{
+        color:'#fff',
+        fontSize:14,
+        fontFamily:"Instrument Sans",
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    text2:{
+      fontSize:16,
+      color:'#ffffffff',
+      alignItems:'center',
+      marginBottom:5,
+      fontFamily:'Josefin Sans',
+      justifyContent:'center',
+      textAlign:'center',
+  },
+});
