502c6911bf8ecb90fd132aae6898912edaba88fb
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-12-01 23:36:11 -0600

    db eventos no se si jale xd

---
+++ redsocial/Models/Evento_Participantes.js
@@ -0,0 +1,7 @@
+export class evento_Participantes {
+    constructor(id_evento_participante, id_evento, id_participante){
+        this.id_evento_participante = id_evento_participante
+        this.id_evento = id_evento
+        this.id_participante = id_participante
+    }
+}
