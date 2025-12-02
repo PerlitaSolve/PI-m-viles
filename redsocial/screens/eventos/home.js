bf736807f2ced2b022e1d0d6fac6fbe9802b67bb
Daniyel19084 <124051408@upq.edu.mx>
2025-12-02 02:34:38 -0600

    Creacion Home

--- redsocial/screens/eventos/home.js
+++ redsocial/screens/eventos/home.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, StyleSheet, View,ScrollView,Pressable,ImageBackground,Image,TextInput } from 'react-native'
+import React, { Component } from 'react'
 
 export default function home() {
-  return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
+
+    return (
+      <ImageBackground
+        source={require('../../assets/Fondo1.png')}
+        style={styles.fondo}      
+      >
+      <ScrollView>
+        
+
+      </ScrollView>
+      </ImageBackground>
+
+
+    )
+  
 }
+
+const styles = StyleSheet.create({
+  fondo:{
+     flex:1,
+     height:'100%',
+     width:'100%',
+     resizeMode:'contain',
+     alignItems:'center',
+     zIndex:0,
+   },
+})
